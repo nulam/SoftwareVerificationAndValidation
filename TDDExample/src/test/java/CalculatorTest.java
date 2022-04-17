47a1b77583b6faa7e1fd6194d71e7b47075bea96
Tomas Halama <nulam@email.cz>
2022-04-17 17:08:01 +0200

    move vals to csv, parametrize the test, refactor error message printing

--- TDDExample/src/test/java/CalculatorTest.java
+++ TDDExample/src/test/java/CalculatorTest.java
@@ -1,11 +1,26 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * SWE 202 - homework 2
+ * id - B211202801
+ * Tomas Halama
+ */
 class CalculatorTest {
+
+    static String templateDivideError = "The result of Calculator.divide method for %f/%f is incorrect. Expected %f, got %f";
+
+    private static String formatDivideError(double input1, double input2, double expected, double output) {
+        return String.format(templateDivideError, input1, input2, expected, output);
+    }
 
     @BeforeEach
     void setUp() {
@@ -15,33 +30,15 @@ class CalculatorTest {
     void tearDown() {
     }
 
-    @Test
-    void testDivision1() {
-        assertEquals(5, Calculator.divide(10, 2));
+    @ParameterizedTest
+    @CsvFileSource(resources = "divide_test_vals.csv")
+    void dataDrivenAdd(double a, double b, double expectedValue) {
+        double res = Calculator.divide(a, b);
+        assertEquals(expectedValue, res, formatDivideError(a, b, expectedValue, res));
     }
 
     @Test
-    void testDivision2() {
-        assertEquals(2.5, Calculator.divide(10, 4));
-    }
-
-    @Test
-    void testDivision3() {
-        assertEquals(2.5, Calculator.divide(12.5, 5));
-    }
-
-    @Test
-    void testDivision4() {
-        assertEquals(4, Calculator.divide(10, 2.5));
-    }
-
-    @Test
-    void testDivision5() {
-        assertEquals(5, Calculator.divide(12.5, 2.5));
-    }
-
-    @Test
-    void testDivision6() {
+    void testDivisionException() {
 
         Exception exception = assertThrows(
                 IllegalArgumentException.class,
